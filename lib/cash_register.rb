d4b261357e134f776e59e8b636b310d88d10f85b
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-19 01:28:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+require 'pry'
+class CashRegister 
+  @@i
+attr_accessor :total, :discount
 
+  def initialize(discounts=0)
+    @total =0
+    @discount=discounts
+  end
+  
+  def total 
+    @total 
+  end 
+  
+  def add_item(title,price,quantity=1)
+    @@items << title
+    @total=total+price*quantity
+  
+  end 
+  
+  def apply_discount
+    if discount!=0 
+      @total=total-(total*discount/100)
+      "After the discount, the total comes to $#{total}."
+    else 
+      "There is no discount to apply."
+    end 
+  end
+  
+  def self.items 
+    @@items
+  end
+end 
